fcc26bb79fc503876c61e164521be34ebec5ef7a
ぺんぎん@TRANs <137734383+zP3n@users.noreply.github.com>
2024-12-09 20:26:32 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,7 @@
 const CONFIG = {
-    API_KEY: process.env.GOOGLE_SHEETS_API_KEY,
-    SPREADSHEET_ID: process.env.GOOGLE_SHEETS_ID,
+    API_KEY: 'AIzaSyCkJvrfnL5yXVJHWXrCmCbeqkR7kMJHfFY',
+    SPREADSHEET_ID: '1JiDNfGzMJL7XrB3JuaCfeD29UDwif3mKopqIJz5f_bA',
     SHEET_NAME: 'Sheet1'
 };
-
-if (!CONFIG.API_KEY || !CONFIG.SPREADSHEET_ID) {
-    console.error('環境変数が設定されていません');
-}
 
 export default CONFIG; 
